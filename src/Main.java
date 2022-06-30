2bb24471d49763c4628d5b87cff23202d1c4c573
VantaWhiteWork <williamwh1te_work@outlook.com>
2022-06-30 12:46:25 +0300

    create ArithmeticProgression and functions: get_progression, get_progression_by_id

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ArithmeticProgression x = new ArithmeticProgression(3);
+        ArithmeticProgression x = new ArithmeticProgression(2);
         System.out.println(x.get_progression_by_id(3));
     }
 }
